cf33e4ccd6c2ebd0b77ff529455e8fb198c07c4c
Zachary Kirtz <zach.kirtz@gmail.com>
2025-07-18 16:26:34 -0500

    bin2hndlr: Convert this to a library (used by wrhndlr).

--- bin2hndlr.py
+++ bin2hndlr.py
@@ -1,22 +1,19 @@
 import sys
 import struct
 
-def main(argv):
-    fp = open(argv[1], "rb")
-    out = open(argv[2], "wb")
-
+def bin_to_core_image(bin_file):
     # Skip over the leader.
     b = 0x80
     while(b == 0x80):
-        b = int(fp.read(1)[0])
+        b = int(bin_file.read(1)[0])
 
-    fp.seek(-1, 1)
+    bin_file.seek(-1, 1)
 
-    data: list = [0] * 0o10000
+    data = bytearray(0o10000)
     skip = False
     address = 0
     while(True):
-        b = int(fp.read(1)[0])
+        b = int(bin_file.read(1)[0])
 
         # Are all bits set? If yes, activate or deactivate skip mode.
         if(b == 0xFF):
@@ -30,7 +27,6 @@ def main(argv):
         # If both bits 7 and 8 are set, this means the data field should be updated
         # But we're not going to worry about that.
         if(b & 0xC0 == 0xC0):
-
             continue
 
         # If we encounter a leader (exclusively bit 7 set), we're done and should exit.
@@ -38,20 +34,18 @@ def main(argv):
             break
 
         # If we encounter a byte with bit 6 set, update the current address.
-        b2 = int(fp.read(1)[0])
+        b2 = int(bin_file.read(1)[0])
         full = ((b & 0x3F) << 6) | (b2 & 0x3F)
         if(b & 0x40):
             address = full
         else:
             # Otherwise? Just a regular word.
-            data[address] = full
+            struct.pack_into("<H", data, address * 2, full)
             address += 1
             if(address >= 0o10000): #address wrap around
                 address = 0
 
-    for num in data:
-        bs = struct.pack("<H", num)
-        out.write(bs)
+    return data
 
-if __name__ == "__main__":
-    exit(main(sys.argv))
+#if __name__ == "__main__":
+#    exit(main(sys.argv))
